be1a71473916da0fe88726dc53710619b2f6c914
WellDone88 <kenneth.moren@live.no>
2025-09-28 15:31:34 +0200

    Update view.js

--- view.js
+++ view.js
@@ -1,7 +1,7 @@
 function updateView(){
 let HTML = document.getElementById("app").innerHTML = /*HTML*/ `
 <div><img id="M8" src="Magic_8_ball.png"></div>
-<h2>Spør et spørsmål høyt og trykk på knappen</h2>
+<h2>Still et spørsmål høyt og trykk på knappen</h2>
 <button onclick="TellFortune()"> Spå meg</button>
 <p id="Answers"><p>
 
